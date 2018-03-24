5e849c77926b3d8a02993637f90a9d777eb98d73
AbhishekChd <acechd97@gmail.com>
2018-03-24 12:40:33 +0530

    Added layout files and initialized

--- app/src/main/java/com/example/abhishek/youtubeapi/MainActivity.java
+++ app/src/main/java/com/example/abhishek/youtubeapi/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.abhishek.youtubeapi;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
@@ -16,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     //Declare View
     private YouTubePlayerSupportFragment mFragment;
 
+    private Button mButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,18 +29,29 @@ public class MainActivity extends AppCompatActivity {
         mFragment = (YouTubePlayerSupportFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.youtube_player);
 
+        mButton = findViewById(R.id.button_play_video);
+
+        initPlayer();
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mFragment.initialize(API_KEY, mOnInitializedListener);
+            }
+        });
+    }
+
+    private void initPlayer() {
         mOnInitializedListener = new YouTubePlayer.OnInitializedListener() {
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-
+                youTubePlayer.loadVideo("SUgI9TQ7JKA");
             }
 
             @Override
             public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-
+                Toast.makeText(MainActivity.this, "Couldn't load video", Toast.LENGTH_SHORT).show();
             }
         };
-
-        mFragment.initialize(API_KEY, mOnInitializedListener);
     }
 }
