9704c14931b163b93b04fd84796c5fc2a4ea8a66
AbhishekChd <acechd97@gmail.com>
2018-03-24 12:28:31 +0530

    Added YouTube listener and Fragment

--- app/src/main/java/com/example/abhishek/youtubeapi/MainActivity.java
+++ app/src/main/java/com/example/abhishek/youtubeapi/MainActivity.java
@@ -1,15 +1,41 @@
 package com.example.abhishek.youtubeapi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String API_KEY = "AIzaSyAQRN9qalqw86alca3D24iXmFJFFE8YCxw";
+    // YouTube Listener
+    private YouTubePlayer.OnInitializedListener mOnInitializedListener;
+
+    //Declare View
+    private YouTubePlayerSupportFragment mFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFragment = (YouTubePlayerSupportFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.youtube_player);
+
+        mOnInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        mFragment.initialize(API_KEY, mOnInitializedListener);
     }
 }
