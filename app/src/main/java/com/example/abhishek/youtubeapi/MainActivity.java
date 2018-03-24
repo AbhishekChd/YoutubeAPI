dae032c2b76e5940188f12af0e2cdd30864539c5
AbhishekChd <acechd97@gmail.com>
2018-03-24 12:09:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/abhishek/youtubeapi/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.abhishek.youtubeapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String API_KEY = "AIzaSyAQRN9qalqw86alca3D24iXmFJFFE8YCxw";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
